403a8d938a7dbe5cc529c4c16c7f9f9a2acb3e91
Ayxhmxn <ayushsurwade30@gmail.com>
2025-04-24 13:24:11 +0530

    changed

--- app/lesson/header.tsx
+++ app/lesson/header.tsx
@@ -1,14 +1,16 @@
 import { InfinityIcon, X } from "lucide-react";
 import { Progress } from "@/components/ui/progress";
 import Image from "next/image";
-
+import { useExitModal } from "@/store/useExitModal";
 type Props = {
     hearts: number;
     percentage: number;
     hasActiveSubscription: boolean;
 };
 
+
 export const Header = ({ hearts, percentage, hasActiveSubscription }: Props) => {
+    const{open} = useExitModal();
     return (
         <header className="lg:pt-[50px] pt-[20px] px-10 flex gap-x-7 items-center justify-between max-w-[1140px] mx-auto w-full">
             <X
