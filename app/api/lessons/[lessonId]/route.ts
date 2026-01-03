b22293ca67cccdd70a2782f66970def950b2d6dc
Your Name <you@example.com>
2026-01-03 18:24:15 +0530

    changes

--- app/api/lessons/[lessonId]/route.ts
+++ app/api/lessons/[lessonId]/route.ts
@@ -3,15 +3,11 @@ import { NextResponse, type NextRequest } from "next/server";
 
 import db from "@/db/drizzle";
 import { lessons } from "@/db/schema";
-import { isAdmin } from "@/lib/admin";
 
 export const GET = async (
   _req: NextRequest,
   { params }: { params: { lessonId: number } }
 ) => {
-  const adminCheck = await isAdmin();
-  if (!adminCheck) return new NextResponse("Unauthorized.", { status: 401 });
-
   const data = await db.query.lessons.findFirst({
     where: eq(lessons.id, params.lessonId),
   });
@@ -23,9 +19,6 @@ export const PUT = async (
   req: NextRequest,
   { params }: { params: { lessonId: number } }
 ) => {
-  const adminCheck = await isAdmin();
-  if (!adminCheck) return new NextResponse("Unauthorized.", { status: 401 });
-
   const body = (await req.json()) as typeof lessons.$inferSelect;
   const data = await db
     .update(lessons)
@@ -42,9 +35,6 @@ export const DELETE = async (
   _req: NextRequest,
   { params }: { params: { lessonId: number } }
 ) => {
-  const adminCheck = await isAdmin();
-  if (!adminCheck) return new NextResponse("Unauthorized.", { status: 401 });
-
   const data = await db
     .delete(lessons)
     .where(eq(lessons.id, params.lessonId))
